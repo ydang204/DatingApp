8d5aa54d99a8a6ab7e54cd596fef0a3bbb69716f
Đặng Huỳnh Đạt Ý <daty.danghuynh20497@gmail.com>
2019-02-26 22:25:11 +0700

    Add User entity and migration

--- DatingApp.Api/Data/DataContext.cs
+++ DatingApp.Api/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace DatingApp.Api.Data
             : base(options) { }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
--- DatingApp.Api/Migrations/20190226152318_AddUserEntity.Designer.cs
+++ DatingApp.Api/Migrations/20190226152318_AddUserEntity.Designer.cs
@@ -4,14 +4,16 @@ using DatingApp.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.Api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190226152318_AddUserEntity")]
+    partial class AddUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
