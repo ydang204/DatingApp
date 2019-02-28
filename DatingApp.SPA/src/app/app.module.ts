70b091ce030aca4d2cb4274071e549e077887c68
Đặng Huỳnh Đạt Ý <daty.danghuynh20497@gmail.com>
2019-02-28 21:04:23 +0700

    Add nav component

--- DatingApp.SPA/src/app/app.module.ts
+++ DatingApp.SPA/src/app/app.module.ts
@@ -6,11 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ValueComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
