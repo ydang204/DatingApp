eae67f4d6cd49202bfdd2c1d98382628ec098b2d
Đặng Huỳnh Đạt Ý <daty.danghuynh20497@gmail.com>
2019-03-03 13:42:17 +0700

    Extended user entity

--- DatingApp.Api/Models/User.cs
+++ DatingApp.Api/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace DatingApp.Api.Models
 {
     public class User
@@ -10,6 +13,24 @@ namespace DatingApp.Api.Models
 
         public byte[] PasswordSalt { get; set; }
 
+        public string Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnowAs { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string Interests { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
         public static User CreateNewUser(string username)
         {
             return new User
@@ -17,6 +38,8 @@ namespace DatingApp.Api.Models
                 Username = username
             };
         }
+
+
 
     }
 }
