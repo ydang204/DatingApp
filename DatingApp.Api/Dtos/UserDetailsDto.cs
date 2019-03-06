702140ea37f0c8c433c46a571787678560591585
Đặng Huỳnh Đạt Ý <daty.danghuynh20497@gmail.com>
2019-03-06 23:58:28 +0700

    Add user edit UI

--- DatingApp.Api/Dtos/UserDetailsDto.cs
+++ DatingApp.Api/Dtos/UserDetailsDto.cs
@@ -24,6 +24,8 @@ namespace DatingApp.Api.Dtos
 
         public string Interests { get; set; }
 
+        public string LookingFor { get; set; }
+
         public string Country { get; set; }
 
         public string City { get; set; }
