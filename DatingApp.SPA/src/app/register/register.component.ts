e8d83374b1351777a97ce126a2d0966a7420d590
Đặng Huỳnh Đạt Ý <daty.danghuynh20497@gmail.com>
2019-03-19 22:29:08 +0700

    Expanding the register form

--- DatingApp.SPA/src/app/register/register.component.ts
+++ DatingApp.SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,26 +9,47 @@ import { AlertifyService } from '../_services/alertify.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  private baseUrl = 'http://localhost:5000/api/auth/register';
-
   @Output() cancleRegister = new EventEmitter();
+  private baseUrl = 'http://localhost:5000/api/auth/register';
+  registerForm: FormGroup;
 
   model: any = {};
 
   constructor(
     private authService: AuthService,
+    private formBuilder: FormBuilder,
     private alertifyService: AlertifyService) { }
 
   ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm(): void {
+    this.registerForm = this.formBuilder.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+        validators: this.passwordMatchValidator
+      });
+  }
+
+  passwordMatchValidator(f: FormGroup) {
+    return f.get('password').value === f.get('confirmPassword').value ? null : { missmatch: true };
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertifyService.success('Registeration successfully');
-    }, err => {
-      this.alertifyService.error(err);
-    });
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertifyService.success('Registeration successfully');
+    // }, err => {
+    //   this.alertifyService.error(err);
+    // });
+    console.log(this.registerForm.value);
   }
 
   cancel() {
