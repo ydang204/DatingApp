10de7897b6af356d4d9cd2e67f1e3196900266d0
Trương Nguyễn Hoàng Thương <truongnguyenhoangthuong97@gmail.com>
2020-04-09 19:50:19 +0700

    DT-9-home-ui

--- src/dating-app-angular-next/src/app/app.module.ts
+++ src/dating-app-angular-next/src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from '@components/layout/layout.module';
 import { SharedModule } from '@components/shared/shared.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, LayoutModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
