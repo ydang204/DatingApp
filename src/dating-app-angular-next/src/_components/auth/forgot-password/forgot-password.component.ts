3dbf6d6a8d6d57aa2be81988f1c9ac1420c32941
VoTanTra <travo9692@gmail.com>
2020-04-09 19:46:15 +0700

    fotgot password UI
    Forgot password UI

--- src/dating-app-angular-next/src/_components/auth/forgot-password/forgot-password.component.ts
+++ src/dating-app-angular-next/src/_components/auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,7 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  forgotpasswordForm: FormGroup;
+  confirmpasswordForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.createForms();
+    this.confirm();
+  }
+
+  createForms(): void {
+    this.forgotpasswordForm = this.fb.group({
+      email: ['', Validators.required]
+    });
+  }
+
+  forgot() {
+    console.log(this.forgotpasswordForm.get('email').value);
+  }
+
+  cancel() {
+    console.log('success cancel');
+  }
+
+  confirm(): void {
+    this.confirmpasswordForm = this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+        validators: this.passwordMatchValidator
+      });
+  }
+
+  passwordMatchValidator(f: FormGroup) {
+    return f.get('password').value === f.get('confirmPassword').value ? null : { missmatch: true };
+  }
+
+  confirmpassword() {
+    console.log('success confirm')
+  }
+
+  verify() {
+    return true;
+  }
+
+  ngOnInit() { }
 }
