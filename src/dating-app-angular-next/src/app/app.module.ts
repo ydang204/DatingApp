76654e4af68d65fbe481923017d31d613cdc977a
DESKTOP-IU2AHER\DELL <trieuvi.dct1155@gmail.com>
2020-04-12 15:51:05 +0700

    Included HttpClient and BrowserAnimationsModule module

--- src/dating-app-angular-next/src/app/app.module.ts
+++ src/dating-app-angular-next/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HomeComponent } from '@components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from '@components/layout/layout.module';
@@ -9,8 +11,8 @@ import { SharedModule } from '@components/shared/shared.module';
 import { RegisterComponent } from '@components/auth/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent,RegisterComponent],
-  imports: [BrowserModule, AppRoutingModule, BsDatepickerModule.forRoot(), LayoutModule, SharedModule],
+  declarations: [AppComponent,RegisterComponent,HomeComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule, BsDatepickerModule.forRoot(), LayoutModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
