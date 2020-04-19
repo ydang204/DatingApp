b04a56cc3dbc64b89c4ff2637ceccd3d439101e1
DESKTOP-IU2AHER\DELL <trieuvi.dct1155@gmail.com>
2020-04-19 16:02:43 +0700

    Added alerityfyjs

--- src/dating-app-angular-next/src/app/app.module.ts
+++ src/dating-app-angular-next/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthService } from '@services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,6 +7,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HomeComponent } from '@components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertifyService } from '@services/alertify.service';
 import { LayoutModule } from '@components/layout/layout.module';
 import { SharedModule } from '@components/shared/shared.module';
 import { RegisterComponent } from '@components/auth/register/register.component';
@@ -13,7 +15,7 @@ import { RegisterComponent } from '@components/auth/register/register.component'
 @NgModule({
   declarations: [AppComponent,RegisterComponent,HomeComponent],
   imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule, BsDatepickerModule.forRoot(), LayoutModule, SharedModule],
-  providers: [],
+  providers: [AlertifyService, AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
