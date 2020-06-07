b4b8c48003e74747845b50edb5937b32fcb9c337
VoTanTra <travo9692@gmail.com>
2020-06-07 20:43:12 +0700

    Member edit profile

--- src/dating-app-angular-next/src/app/app.module.ts
+++ src/dating-app-angular-next/src/app/app.module.ts
@@ -2,20 +2,56 @@ import { AuthService } from '@services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TimeAgoPipe } from 'time-ago-pipe';
+
+
 import { HomeComponent } from '@components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlertifyService } from '@services/alertify.service';
-import { LayoutModule } from '@components/layout/layout.module';
 import { SharedModule } from '@components/shared/shared.module';
 import { RegisterComponent } from '@components/auth/register/register.component';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../_components/layout/navbar/navbar.component';
+import { MemberEditComponent } from '../_components/members/member-edit/member-edit.component';
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
-  declarations: [AppComponent,RegisterComponent,HomeComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule, BsDatepickerModule.forRoot(), LayoutModule, SharedModule],
+  declarations: [AppComponent, RegisterComponent, HomeComponent, NavbarComponent, MemberEditComponent, TimeAgoPipe],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BsDatepickerModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    FontAwesomeModule,
+    SharedModule,
+    JwtModule.forRoot({
+      // Use this config to globally add jwt to request
+      // https://github.com/auth0/angular2-jwt#usage-injection
+      config: {
+         tokenGetter,
+         whitelistedDomains: ['localhost:5000'],
+         blacklistedRoutes: ['localhost:5000/api/auth']
+      }
+   })
+  ],
   providers: [AlertifyService, AuthService],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/dating-app-angular-next/src/app/routes.ts
+++ src/dating-app-angular-next/src/app/routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '@components/home/home.component';
+import { RegisterComponent } from '../_components/auth/register/register.component';
+import { MemberEditComponent } from '../_components/members/member-edit/member-edit.component';
+import { PreventUnsavedChanges } from "../_guards/prevent-unsaved-changes.guard";
+import { MemberEditResolver } from "../_resolver/member-edit.resolver";
 
 
 export const appRoutes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: '**', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'members/edit', component: MemberEditComponent, resolve: { user: MemberEditResolver },
+    canDeactivate: [PreventUnsavedChanges]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
